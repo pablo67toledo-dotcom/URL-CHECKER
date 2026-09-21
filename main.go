ed695f9455023f17c813770add7d45dff9eca56f
pablo67toledo-dotcom <pablo67toledo@gmail.com>
2026-09-21 19:14:36 -0300

    Feat: valida URL recebida

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // Função para analisar a URL recebida
 func analisarURL(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("url")
-	fmt.Fprintln(w, "URL recebida:", url)
+	urlRecebida := r.URL.Query().Get("url")
+
+	_, err := url.ParseRequestURI(urlRecebida)
+
+	if err != nil {
+		http.Error(w, "URL inválida", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "URL válida:", urlRecebida)
 }
 
 // Função principal do programa
